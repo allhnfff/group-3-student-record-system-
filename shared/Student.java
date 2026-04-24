44a4affbd8a891e0feaa7cac487a5417831b715c
hanif <hanifmuslim998@gmail.com>
2026-04-24 08:53:35 +0700

    Tambah shared Student.java

--- shared/Student.java
+++ shared/Student.java
@@ -1,12 +1,31 @@
 package shared;
 
-public class Student{
+public class Student {
+    private String nim;
+    private String name;
+    private String major;
+    private int year;
 
-    public String nim;
-    public String name;
-
-    public Student(String nim, String name) {
+    public Student(String nim, String name, String major, int year) {
         this.nim = nim;
         this.name = name;
+        this.major = major;
+        this.year = year;
+    }
+
+    public String getNim() {
+        return nim;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public int getYear() {
+        return year;
     }
 }
