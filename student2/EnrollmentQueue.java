a33775af49c9e441308c510ca1401224f3f84237
tia eryanti <tiaeryanti8@gmail.com>
2026-04-24 23:09:34 +0700

    menyelesaikan tugas antrian mahasiswa

--- student2/EnrollmentQueue.java
+++ student2/EnrollmentQueue.java
@@ -1,5 +1,43 @@
 package student2;
 
+import java.util.LinkedList;
+
 public class EnrollmentQueue {
-    
+    private LinkedList<Student> queue;
+
+    public EnrollmentQueue() {
+        queue = new LinkedList<>();
+    }
+
+    public void enqueue(Student student) {
+        queue.addLast(student);
+        System.out.println(student.getName() + " masuk antrian");
+    }
+    public void dequeue() {
+        if (queue.isEmpty()) {
+            System.out.println("Antrian kosong");
+        } else {
+            Student s = queue.removeFirst();
+            System.out.println(s.getName() + " keluar dari antrian");
+        }
+    }
+    public void peek() {
+        if (queue.isEmpty()) {
+            System.out.println("Antrian kosong");
+        } else {
+            System.out.println("Antrian terdepan: " + queue.getFirst().getName());
+        }
+    }
+    public void displayQueue() {
+        if (queue.isEmpty()) {
+            System.out.println("Antrian kosong");
+        } else {
+            System.out.println("isi antrian:");
+            for (Student s : queue) {
+                System.out.println(s.getName());
+            }
+        }
+    }
 }
+
+
--- student2/EnrollmentQueueDemo.java
+++ student2/EnrollmentQueueDemo.java
@@ -1,5 +1,21 @@
 package student2;
 
 public class EnrollmentQueueDemo {
-    
+    public static void main(String[] args) {
+        EnrollmentQueue queue = new EnrollmentQueue();
+
+        Student s1 = new Student( "001", "syifa", "SI", 2023);
+        Student s2 = new Student( nim: "002", "Budi", "TI", 2022);
+        Student s3 = new Student( "003", "Lani", "TI", 2025);
+
+        queue.enqueue(s1);
+        queue.enqueue(s2);
+        queue.enqueue(s3);
+
+        queue.displayQueue();
+
+        queue.dequeue();
+
+        queue.peek();
+    }  
 }
