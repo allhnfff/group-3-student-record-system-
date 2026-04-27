8692e5500b8ed7a98cabd70b206b0c5406dff7d3
maysafrianto <maysafrianto841@gmail.com>
2026-04-27 17:12:46 +0700

    final fix update

--- student5/EnrollmentDequeDemo.java
+++ student5/EnrollmentDequeDemo.java
@@ -1,5 +1,30 @@
 package student5;
 
+import shared.Student;
+
 public class EnrollmentDequeDemo {
+
+    public static void main(String[] args) {
+        EnrollmentDeque deque = new EnrollmentDeque();
+
+        // Data sesuai soal
+        deque.addRear(new Student("001", "Fajar Nugroho", "TI", 2023));
+        deque.addRear(new Student("002", "Laila Fitriani", "TI", 2023));
+        deque.addFront(new Student("003", "Rizky Pratama", "TI", 2023));
+
+        // Display awal
+        deque.display();
+
+        // Peek 
+        System.out.println("Prioritas: " + deque.peekFront().getName());
     
+        // Remove
+        deque.removeFront();
+        deque.removeRear();
+
+        // Display akhir
+        deque.display();
+
+    }
+
 }
