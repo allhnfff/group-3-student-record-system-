c6341c29d97ffbd9d7b2536777176e65922105e8
tia eryanti <tiaeryanti8@gmail.com>
2026-08-10 09:48:50 +0700

    Implementasi PriorityEnrollmentQueue dan Demo UAS

--- student2/PriorityEnrollmentQueueDemo.java
+++ student2/PriorityEnrollmentQueueDemo.java
@@ -1,5 +1,27 @@
 package student2;
 
+import shared.Student;
+
 public class PriorityEnrollmentQueueDemo {
+    public static void main(String[] args) {
+        PriorityEnrollmentQueue pq = new PriorityEnrollmentQueue();
+
+        // Menggunakan constructor Student(id,name)
+        pq.enqueue(new Student("NIM003", "Rizky"), 3.45);
+        pq.enqueue(new Student("NIM001", "Fajar"), 3.80);
+        pq.enqueue(new Student("NIM004", "Hana"), 2.95);
+
+        //cetak daftar antrian (terurut dari IPK tertinggi)
+        pq.display();
+
+        System.out.println();
+        
+        // Dequeue sesuai urutan prioritas IPK
+        pq.dequeueMax();
+        pq.dequeueMax();
+        pq.dequeueMax();
+
     
+    }
 }
+
